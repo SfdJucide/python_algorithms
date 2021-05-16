28ae20b17735c2d0f92912004a1cb53b05c97d32
sfd <kater167761@gmail.com>
2021-05-16 16:33:04 +0300

    add first homework

---
+++ 6.py
@@ -0,0 +1,9 @@
+"""
+https://drive.google.com/file/d/1sNVWqjeOPcVQBo_-NtI3TbDuWVp1naO8/view?usp=sharing
+начало
+ввод номера буквы(n)
+вывод(перевод n+96 в букву алфавита согласно таблице ASCII)
+конец
+"""
+n = int(input("Введите номер буквы в алфавите "))
+print(f"Это буква {chr(n + 96)}")
